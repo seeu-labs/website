02d398f4b26485211c1c401416aabba860d3c378
waite <waite1x@outlook.com>
2023-12-27 08:05:25 +0800

    feat: Add market cap dashboard item

--- website/src/pages/Mint/Dashboard.tsx
+++ website/src/pages/Mint/Dashboard.tsx
@@ -1,9 +1,25 @@
 import api, { SeeUDetail } from 'api'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import bitcoin from 'assets/images/bitcoin.png'
 
 export function BitcoinIcon() {
   return <img className="mx-2 w-[28px]" src={bitcoin} alt="bitcoin" />
+}
+
+function DashbooardItem(props: {
+  value: string | React.ReactNode
+  label: string
+}) {
+  return (
+    <div className="min-w-[180px] rounded-xl p-4 shadow-xl">
+      <div className="flex items-center justify-center text-2xl font-extrabold">
+        {props.value}
+      </div>
+      <div className="mt-1 text-center text-lg text-gray-600">
+        {props.label}
+      </div>
+    </div>
+  )
 }
 
 export function Dashbooard() {
@@ -17,38 +33,45 @@ export function Dashbooard() {
   }, [])
   return data ? (
     <div className="m-auto flex flex-wrap gap-8">
-      <div className="min-w-[200px] rounded-xl p-4 shadow-lg">
-        <div className="flex items-center justify-center text-3xl font-bold">
-          <BitcoinIcon />
-          {Number.parseFloat(data.marketplace.totalVolume).toFixed(4)}
-        </div>
-        <div className="mt-1 text-center text-lg text-gray-600">
-          Total volume
-        </div>
-      </div>
-      <div className="min-w-[200px] rounded-xl p-4 text-center shadow-md">
-        <div className="flex items-center justify-center text-3xl font-bold">
-          <BitcoinIcon />
-          {Number.parseFloat(data.marketplace.volume24h).toFixed(4)}
-        </div>
-        <div className="mt-1 text-center text-lg text-gray-600">24h Volume</div>
-      </div>
-      <div className="min-w-[200px] rounded-xl p-4 shadow-md">
-        <div className="flex items-center justify-center text-3xl font-bold">
-          {(Number.parseFloat(data.marketplace.floorPrice) * 100000000).toFixed(
+      <DashbooardItem
+        label="Total volume"
+        value={
+          <>
+            <BitcoinIcon />
+            {Number.parseFloat(data.marketplace.totalVolume).toFixed(4)}
+          </>
+        }
+      />
+      <DashbooardItem
+        label="24h Volum"
+        value={
+          <>
+            <BitcoinIcon />
+            {Number.parseFloat(data.marketplace.volume24h).toFixed(4)}
+          </>
+        }
+      />
+      <DashbooardItem
+        label="Market cap"
+        value={
+          <>
+            <BitcoinIcon />
+            {(
+              Number.parseFloat(data.marketplace.floorPrice) *
+              Number.parseFloat(data.detail.totalSupply)
+            ).toFixed(4)}
+          </>
+        }
+      />
+      <DashbooardItem
+        label="Floor Price"
+        value={
+          (Number.parseFloat(data.marketplace.floorPrice) * 100000000).toFixed(
             4
-          ) + ' sats'}
-        </div>
-        <div className="mt-1 text-center text-lg text-gray-600">
-          Floor price
-        </div>
-      </div>
-      <div className="min-w-[200px] rounded-xl p-4 shadow-md">
-        <div className="flex items-center justify-center text-3xl font-bold">
-          {data.detail.holder}
-        </div>
-        <div className="mt-1 text-center text-lg text-gray-600">Holders</div>
-      </div>
+          ) + ' sats'
+        }
+      />
+      <DashbooardItem value={data.detail.holder} label="Hodlers" />
     </div>
   ) : null
 }
