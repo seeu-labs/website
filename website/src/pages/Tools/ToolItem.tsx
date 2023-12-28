f9089cb61ce8645a869da43047b34f78089eb89b
waite <waite1x@outlook.com>
2023-12-28 17:06:09 +0800

    feat: 网站全新改版 (#10)

---
+++ website/src/pages/Tools/ToolItem.tsx
@@ -0,0 +1,27 @@
+import { Tool } from '.'
+
+export function ToolItem(props: { value: Tool; group: string }) {
+  return (
+    <div
+      onClick={() => {
+        window.open(props.value.url, '_blank')
+      }}
+      className="flex cursor-pointer flex-col overflow-hidden rounded-lg bg-[#1d1e1f] shadow-[0_4px_6px_-1px_rgba(0,0,0,1)]"
+    >
+      <img
+        src={props.value.image}
+        alt={props.value.image}
+        className="h-full w-full flex-1 object-cover"
+      />
+      <div className="flex items-center justify-between p-[16px]">
+        <div>{props.value.url}</div>
+        {/* <Tag
+          className="px-[8px] py-[4px] text-sm"
+          color={colors[Math.floor(Math.random() * colors.length)]}
+        >
+          {props.group}
+        </Tag> */}
+      </div>
+    </div>
+  )
+}
--- website/src/pages/Tools/index.tsx
+++ website/src/pages/Tools/index.tsx
@@ -6,59 +6,59 @@ import tool5 from 'assets/images/tool5.png'
 import tool6 from 'assets/images/tool6.png'
 import tool7 from 'assets/images/tool7.png'
 
-type Tool = {
+export type Tool = {
   id: number
   url: string
   image: string
 }
 
-const Tools = () => {
-  const tools: {
-    [x: string]: Tool[]
-  } = {
-    dashboard: [
-      {
-        id: 1,
-        url: 'https://mempool.space/',
-        image: tool1
-      },
-      {
-        id: 2,
-        url: 'https://dune.com/cryptokoryo/brc20',
-        image: tool2
-      },
-      {
-        id: 3,
-        url: 'https://mct.xyz/inscribe/monitor?inscribe_type=brc20-mint&block_type=pending',
-        image: tool3
-      },
-      {
-        id: 4,
-        url: 'https://ordinals.hiro.so/',
-        image: tool4
-      }
-    ],
-    'Mint Tools': [
-      {
-        id: 1,
-        url: 'https://bitpunks.io/Punk/Insbot',
-        image: tool5
-      },
-      {
-        id: 2,
-        url: 'https://hub.cryptocell.guru/ordinalsturbo/',
-        image: tool6
-      }
-    ]
-  }
+export const toolsData: {
+  [x: string]: Tool[]
+} = {
+  dashboard: [
+    {
+      id: 1,
+      url: 'https://mempool.space/',
+      image: tool1
+    },
+    {
+      id: 2,
+      url: 'https://dune.com/cryptokoryo/brc20',
+      image: tool2
+    },
+    {
+      id: 3,
+      url: 'https://mct.xyz/inscribe/monitor?inscribe_type=brc20-mint&block_type=pending',
+      image: tool3
+    },
+    {
+      id: 4,
+      url: 'https://ordinals.hiro.so/',
+      image: tool4
+    }
+  ],
+  'Mint Tools': [
+    {
+      id: 1,
+      url: 'https://bitpunks.io/Punk/Insbot',
+      image: tool5
+    },
+    {
+      id: 2,
+      url: 'https://hub.cryptocell.guru/ordinalsturbo/',
+      image: tool6
+    }
+  ]
+}
 
+export const Tools = () => {
   return (
     <div className="flex w-full flex-col items-center justify-between gap-[36px] overflow-auto px-8 pb-10 pt-[64px]">
-      {Object.keys(tools).map((key) => (
+      {Object.keys(toolsData).map((key) => (
         <div key={key}>
           <h1 className="mb-[8px] text-[24px] font-[600] capitalize">{key}</h1>
           <div className="grid grid-cols-1 gap-[36px] xl:grid-cols-2">
-            {tools[key].map((tool) => (
+            {toolsData[key].map((tool) => (
               <a href={tool.url} target="_blank" rel="noreferrer" key={tool.id}>
                 <div className="col-span-1 rounded-lg p-4 shadow hover:shadow-lg">
                   <div className="line-wrap one-line-wrap mb-[8px] block">
